f969d4a6f5db3ff507a87a1d05f94f1472befc41
Bafedile <Ddimakatso30@gmail.com>
2023-02-17 17:20:33 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <div class="container"><h1>Portfolio coming soon</h1></div> `,
+  styles: [
+    `
+      .container {
+        align-items: 'center';
+        justify-content: 'center';
+    
+        background:#818382;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'PersonalPortfolio';
