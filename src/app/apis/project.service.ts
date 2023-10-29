0f861c65ad349835a6de00cf1ab755c464c4d27c
Bafedile <Ddimakatso30@gmail.com>
2023-10-29 10:34:27 +0200

    updated projects

--- src/app/apis/project.service.ts
+++ src/app/apis/project.service.ts
@@ -6,11 +6,18 @@ import { Project } from 'src/models';
 export class ProjectService {
   projects: Project[] = [
     {
-      name: 'Marvel',
+      name: 'Marvel  Explorer',
       description:
         'This project provides a platform where you can explore a vast collection of Marvel characters and movies. Dive into the Marvel Universe and discover the iconic heroes and thrilling cinematic adventures that have captivated fans worldwide.',
       link: 'https://marvel.blxckcicada.co.za',
       image: 'assets/marvel_project.png',
+    },
+    {
+      name: 'Animal Kingdom',
+      description:
+        'The Animal Kingdom Explorer is a project designed to help you explore and learn about various types of animals, classified as herbivores, carnivores, and omnivores. Immerse yourself in the world of the animal kingdom, discovering their unique characteristics and dietary preferences.',
+      link: 'https://animals.blxckcicada.co.za',
+      image: 'assets/animals_project.png',
     },
   ];
 
